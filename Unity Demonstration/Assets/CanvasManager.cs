547b6d2a7c3b11a382fcb48fd1758defdefe305b
Liam Harrison <liam.harrison12345@gmail.com>
2019-07-24 18:01:33 +1000

    Adding UI and a Controller for rendering mode.

---
+++ Unity Demonstration/Assets/CanvasManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Canvas))]
+public class CanvasManager : MonoBehaviour
+{
+
+    #region Singleton
+    private static CanvasManager instance;
+    public static CanvasManager Instance
+    {
+        get
+        {
+            return instance ?? FindObjectOfType<CanvasManager>();
+        }
+    }
+    #endregion
+
+    private void Awake()
+    {
+        instance = instance ?? this;
+    }
+
+    private Canvas canvas;
+    void Start()
+    {
+        canvas = GetComponent<Canvas>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public static RenderMode ScreenMode
+    {
+        get
+        {
+            return Instance.canvas.renderMode;
+        }
+        set
+        {
+            Instance.canvas.renderMode = value;
+        }
+    }
+}
